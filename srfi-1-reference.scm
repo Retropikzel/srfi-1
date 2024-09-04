a6825b1d81f96a77a1623b601b06e842fded459d
retropikzel <retropikzel@iki.fi>
2024-09-04 15:37:31 +0300

    Add missing utils

--- srfi-1-reference.scm
+++ srfi-1-reference.scm
@@ -211,6 +211,57 @@
 ;;; The SRFI discussion record contains more discussion on this topic.
 
 
+;;; Utilities
+
+(define (check-arg pred val caller)
+    (if (pred val) val (error "Bad argument" val pred caller)))
+
+(define optional (lambda (a b) (if (null? a) b (car a))))
+
+(define-syntax let-optionals
+      (syntax-rules ()
+        ((let-optionals arg (opt-clause ...) body ...)
+         (let ((rest arg))
+           (internal-let-optionals rest (opt-clause ...) body ...)))))
+
+(define-syntax internal-let-optionals
+      (syntax-rules ()
+        ((internal-let-optionals arg (((var ...) xparser) opt-clause ...) body ...)
+         (call-with-values (lambda () (xparser arg))
+                           (lambda (rest var ...)
+                             (internal-let-optionals rest (opt-clause ...) body ...))))
+
+        ((internal-let-optionals arg ((var default) opt-clause ...) body ...)
+         (call-with-values (lambda () (if (null? arg) (values default '())
+                                        (values (car arg) (cdr arg))))
+                           (lambda (var rest)
+                             (internal-let-optionals rest (opt-clause ...) body ...))))
+
+        ((internal-let-optionals arg ((var default test) opt-clause ...) body ...)
+         (call-with-values (lambda ()
+                             (if (null? arg) (values default '())
+                               (let ((var (car arg)))
+                                 (if test (values var (cdr arg))
+                                   (error "arg failed LET-OPT test" var)))))
+                           (lambda (var rest)
+                             (internal-let-optionals rest (opt-clause ...) body ...))))
+
+        ((internal-let-optionals arg ((var default test supplied?) opt-clause ...) body ...)
+         (call-with-values (lambda ()
+                             (if (null? arg) (values default #f '())
+                               (let ((var (car arg)))
+                                 (if test (values var #t (cdr arg))
+                                   (error "arg failed LET-OPT test" var)))))
+                           (lambda (var supplied? rest)
+                             (internal-let-optionals rest (opt-clause ...) body ...))))
+
+        ((internal-let-optionals arg (rest) body ...)
+         (let ((rest arg)) body ...))
+
+        ((internal-let-optionals arg () body ...)
+         (if (null? arg) (begin body ...)
+           (error "Too many arguments in let-opt" arg)))))
+
 ;;; Constructors
 ;;;;;;;;;;;;;;;;
 
